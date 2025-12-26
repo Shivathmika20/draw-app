d8dfbc992c3c70554e156894d009eb3a3612a2d0
Shivathmika20 <yelurishivathmika@gmail.com>
2025-12-26 20:13:39 +0530

    fix: regenerated prisma client

--- apps/http-backend/src/controllers/auth.ts
+++ apps/http-backend/src/controllers/auth.ts
@@ -2,15 +2,23 @@ import { Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '@repo/backend-auth/config';
 import { SignupSchema,SigninSchema } from '@repo/common-types/types';
-
+import { prisma } from '@repo/db/client';
 
 
 export const signup = async (req: Request, res: Response) => {
-    const data=SignupSchema.safeParse(req.body);
-    if(!data.success){
+    const parsedData=SignupSchema.safeParse(req.body);
+    if(!parsedData.success){
         return res.status(400).json({message:'Invalid data'});
     }
 //db call
+    await prisma.user.create({
+        data:{
+            username:parsedData.data.username,
+            password:parsedData.data.password,
+            name:parsedData.data.name,
+            
+        }
+    })
 
     res.json({message:'User created successfully'});
 }
