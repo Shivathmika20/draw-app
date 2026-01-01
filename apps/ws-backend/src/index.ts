33d7d693233304e558eae1429b034437361796cd
Shivathmika20 <yelurishivathmika@gmail.com>
2026-01-01 13:04:16 +0530

    feat: implemented websocket connection

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -5,6 +5,23 @@ import { JWT_SECRET } from '@repo/backend-auth/config';
 
 const wss = new WebSocketServer({ port: 8080 });
 
+interface User{
+  ws:WebSocket,
+  userId:string,
+  rooms:string[],
+}
+
+const users:User[]=[]
+
+function checkUser(token: string): string | null {
+  const decoded = jwt.verify(token, JWT_SECRET);
+  if (!decoded || typeof decoded === 'string' || !decoded.userId) {
+    return null;
+    }
+    return decoded.userId;
+}
+  
+
 wss.on('connection', function connection(ws,req) {
   console.log('Client connected');
   const url=req.url || ''; //ws://localhost:8080?token='12232'
@@ -12,18 +29,20 @@ wss.on('connection', function connection(ws,req) {
     return;
   }
   const urlParams=new URLSearchParams(url.split('?')[1]);
-  const token=urlParams.get('token');
-  if(!token){
-    return;
+  const token=urlParams.get('token') || '';
+  const userId=checkUser(token);
+  if(!userId){
+    ws.close();
+    return null;
   }
-  const decoded=jwt.verify(token ,JWT_SECRET);
-  if(!decoded || typeof decoded === 'string' || !decoded.userId){
-    ws.close()
-    return
-  }
+
+  users.push({
+   userId,
+   rooms:[],
+   ws:ws as unknown as WebSocket 
+  })
+
   
-
-
   ws.on('message', function message(data) {
     ws.send("pong")
   });
